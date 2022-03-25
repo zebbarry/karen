f035fa0edce4b24819f2920567d6da5dce40caa8
Zeb Barry <zba24@uclive.ac.nz>
2022-03-25 18:50:00 +1300

    Update chore movement to allow rotating past start/beginning

--- app/chores/index.js
+++ app/chores/index.js
@@ -1,54 +1,62 @@
-const moveChoreUp = (row) => {
-  previous_row = row.previousElementSibling;
-  previous_chore = previous_row.getElementsByClassName("chore")[0];
-  current_chore = row.getElementsByClassName("chore")[0];
-  temp = previous_chore.textContent;
-  previous_chore.textContent = current_chore.textContent;
-  current_chore.textContent = temp;
+const calcNewIndex = (current, direction, min, max) => {
+  const new_index = current + direction;
+  if (new_index > max) {
+    return min;
+  }
+  if (new_index < min) {
+    return max;
+  }
+  return new_index;
 };
 
-const moveChoreDown = (row) => {
-  previous_row = row.nextElementSibling;
-  previous_chore = previous_row.getElementsByClassName("chore")[0];
-  current_chore = row.getElementsByClassName("chore")[0];
-  temp = previous_chore.textContent;
-  previous_chore.textContent = current_chore.textContent;
-  current_chore.textContent = temp;
+const moveCellContent = (cells, current_index, direction) => {
+  const new_index = calcNewIndex(current_index, direction, 0, cells.length - 1);
+  const current_cell = cells[current_index].getElementsByClassName("chore")[0];
+  const new_cell = cells[new_index].getElementsByClassName("chore")[0];
+  // Swap text content
+  [current_cell.textContent, new_cell.textContent] = [
+    new_cell.textContent,
+    current_cell.textContent,
+  ];
 };
 
-const chores_table = document.getElementById("chore-table");
-
-for (const row of chores_table.getElementsByClassName("chore-pair")) {
-  let up = row.getElementsByClassName("up")[0];
+const addMoveCallbacks = (row, index) => {
+  const up = row.getElementsByClassName("up")[0];
   up.addEventListener("click", () => {
-    moveChoreUp(row);
+    moveCellContent(chore_pairs, index, -1);
   });
-  let down = row.getElementsByClassName("down")[0];
+  const down = row.getElementsByClassName("down")[0];
   down.addEventListener("click", () => {
-    moveChoreDown(row);
+    moveCellContent(chore_pairs, index, +1);
   });
-}
+};
 
-const compileChores = (table) => {
+const compileChores = (chores_pairs) => {
   let chores = {};
-  for (const row of table.getElementsByClassName("chore-pair")) {
-    chore = row.getElementsByClassName("chore")[0];
-    person = row.getElementsByClassName("person")[0];
+  for (const pair of chores_pairs) {
+    const chore = pair.getElementsByClassName("chore")[0];
+    const person = pair.getElementsByClassName("person")[0];
     chores[chore.textContent] = person.textContent;
   }
   return chores;
 };
 
+const edit_button = document.getElementById("edit");
+const save_button = document.getElementById("save");
+const modify_cells = document.getElementsByClassName("modify-chore");
+
+const chores_table = document.getElementById("chore-table");
+const chore_pairs = chores_table.getElementsByClassName("chore-pair");
+
+[...chore_pairs].forEach(addMoveCallbacks);
+
 const setModifyState = (enabled) => {
   edit_button.disabled = enabled;
   save_button.disabled = !enabled;
-  for (const cell of document.getElementsByClassName("modify-chore")) {
+  for (const cell of modify_cells) {
     cell.hidden = !enabled;
   }
 };
-
-const edit_button = document.getElementById("edit");
-const save_button = document.getElementById("save");
 
 edit_button.addEventListener("click", () => {
   setModifyState(true);
@@ -56,8 +64,8 @@ edit_button.addEventListener("click", () => {
 
 save_button.addEventListener("click", () => {
   setModifyState(false);
-  var xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   xhr.open("PUT", "/update-chores", true);
   xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify(compileChores(chores_table)));
+  xhr.send(JSON.stringify(compileChores(chore_pairs)));
 });
